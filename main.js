175530af01fadd9787c27ec4b5f64650a851ed15
YosepRA <yosep.ryan.yra@gmail.com>
2021-08-31 19:33:19 +0700

    Trigger Github Actions.

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ const app = document.getElementById('app');
 function init() {
   const content = `
     <h1>Hello from Vite</h1>
+    <h1>This will be a change to trigger Github Actions.</h1>
     <p>Today is ${getDay()}</p>
   `;
 
